08e57f8185b8549c6582cceb5713f3d87c9f1cd8
Sven Riedel <sr@gimp.org>
2018-12-26 09:17:57 +0100

    Document usage

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 MockHttpServer
 ==============
 
-** TODO: Add description **
+A mock http server for unit tests, where responses can be triggered not by
+matching with a URL, but by requesting a specific response with a value in the
+HTTP request headers.
+
+## Usage
+Starting up the mock server:
+
+    MockHttpServer.RegistrationService.start\_link
+
+Register your responses:
+  
+    response_code = 200
+    response_headers = [ { "x-my-header", "my value" } ]
+    response_body = "I am a body"
+
+    transaction_id = MockHttpServer.RegistrationService.register( { response_code, response_headers, response_body } )
+
+Trigger a canned response to a query:
+
+    use Plug.Test
+    { returned_response_code,
+      returned_response_headers,
+      returned_response_body } = conn( :get, "/some/path", "" )
+                                 |> put_req_header( "x-mock-tid", transaction_id )
+                                 |> MockHttpServer.HttpServer.call
+                                 |> sent_resp
+
+By default, a non-mocked request will return `{ 999, [ { "cache-control", "max-age=0, private, must-revalidate" } ], "" ]`.
+
+This response can be changed by registering a default action:
+
+  MockHttpServer.RegistrationService.register( { 404, [], "Whatcha talkin about, Willis?" } )
+    { returned_response_code,
+      returned_response_headers,
+      returned_response_body } = conn( :get, "/some/path", "" )
+                                 |> MockHttpServer.HttpServer.call
+                                 |> sent_resp
+    
+    ^returned_response_code = 404
+    ^returned_response_headers = [ { "cache-control", "max-age=0, private, must-revalidate" } ]
+    ^returned_response_body = "Whatcha talkin about, Willis?"
+
+When done, shut down the registration server:
+
+    MockHttpServer.RegistrationService.stop
--- test/mock_http_server_test.exs
+++ test/mock_http_server_test.exs
@@ -1,7 +1,6 @@
 defmodule MockHttpServerTest do
   use ExUnit.Case
   use Plug.Test
-  import MockHttpServer.HttpServer
 
   test "requesting a set-up mocked test url by tid" do
     { :ok, _ } = MockHttpServer.RegistrationService.start_link
@@ -26,5 +25,41 @@ defmodule MockHttpServerTest do
     { "x-foo", "bar" } = Enum.find( true_response_headers, nil, fn({k, _v}) -> "x-foo" == k end )
   end
 
-  # test "requesting a non-mocked test url"
+  test "requesting a non-mocked test url" do
+    { :ok, _ } = MockHttpServer.RegistrationService.start_link
+
+    { true_response_code,
+      true_response_headers,
+      true_response_body } = conn( :get, "/some/path", "" ) 
+                             |> MockHttpServer.HttpServer.call
+                             |> sent_resp
+    
+    MockHttpServer.RegistrationService.stop
+
+    assert true_response_code == 999
+    assert true_response_headers == [ { "cache-control", "max-age=0, private, must-revalidate" } ]
+    assert true_response_body == ""
+  end
+
+  test "requesting a non-mocked test url after changing the default response" do
+    { :ok, _ } = MockHttpServer.RegistrationService.start_link
+    default_response_code = 404
+    default_response_headers = []
+    default_response_body = "Foo"
+
+    default_response = { default_response_code, default_response_headers, default_response_body }
+
+    MockHttpServer.RegistrationService.register_default_action( default_response )
+    { mocked_response_code,
+      mocked_response_headers,
+      mocked_response_body } = conn( :get, "/some/path", "" ) 
+                               |> MockHttpServer.HttpServer.call
+                               |> sent_resp
+    
+    MockHttpServer.RegistrationService.stop
+
+    assert mocked_response_code == default_response_code
+    assert mocked_response_headers == [ { "cache-control", "max-age=0, private, must-revalidate" } ]
+    assert mocked_response_body == default_response_body
+  end
 end
