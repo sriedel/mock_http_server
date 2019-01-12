62effe2128e70f726163e4195fd0a0e1fc5e6ded
Sven Riedel <sr@gimp.org>
2019-01-12 10:19:21 +0100

    Bump to 0.2.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MockHttpServer.Mixfile do
 
   def project do
     [app: :mock_http_server,
-     version: "0.2.3",
+     version: "0.2.4",
      elixir: "~> 1.7",
      build_embedded:  Mix.env == :prod,
      start_permanent: Mix.env == :prod,
